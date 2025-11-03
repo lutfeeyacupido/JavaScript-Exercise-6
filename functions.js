8b1c8fc508d79509541bd27cccbc2b52951e6e1d
lutfeeyacupido <cupidlikeperson@gmail.com>
2025-11-03 15:04:07 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+function add(x, y) {
+    return x + y;
+}
+
+const greeting = greet("Feeya");
+const sum = add(4, 2);
+
+console.log(greeting);
+console.log("This is the result of the sum:", sum);
